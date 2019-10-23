7ed26898f27e38694cc4c6cf6885612a6a252e76
Romain Laforgue <rolaforg@z2r5p2.le-101.fr>
2019-10-23 15:01:08 +0200

    ✅ ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: rolaforg <rolaforg@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/08 21:34:47 by rolaforg     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/22 13:29:46 by rolaforg    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/23 14:58:56 by rolaforg    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,17 +15,14 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t		i;
 	char		*d;
 	const char	*s;
 
-	i = 0;
 	d = dst;
 	s = src;
-	while (i < n && s[i])
-	{
-		d[i] = s[i];
-		i++;
-	}
+	if (!dst && !src)
+		return (NULL);
+	while (n--)
+		*d++ = *s++;
 	return (dst);
 }
