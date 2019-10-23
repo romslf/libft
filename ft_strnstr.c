a6859d2211e4e9a74c63fe320cdf4039f0b441fd
Romain Laforgue <rolaforg@z2r5p2.le-101.fr>
2019-10-23 18:04:45 +0200

    ✅ ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                              /             */
 /*   ft_strnstr.c                                     .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: rolaforg <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*   By: rolaforg <rolaforg@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/10 14:55:35 by rolaforg     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/10 15:06:47 by rolaforg    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/23 18:02:51 by rolaforg    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,28 +16,22 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
-	int		x;
-	int		pos;
+	size_t	j;
 
 	i = 0;
-	x = 0;
-	pos = 0;
-	while (haystack[i] && needle[x] && i < len)
+	j = 0;
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] && i < len)
 	{
-		if (haystack[i] == needle[x])
+		j = 0;
+		while (haystack[i + j] == needle[j] && i + j < len)
 		{
-			if (x == 0)
-				pos = i;
-			x++;
-		}
-		else if (haystack[i] != needle[i] && x != 0)
-		{
-			x = 0;
-			pos = 0;
+			if (needle[j + 1] == '\0')
+				return ((char *)haystack + i);
+			j++;
 		}
 		i++;
 	}
-	if ((x == 0 && needle[0]) || needle[x] != '\0')
-		return (NULL);
-	return ((char *)haystack + pos);
+	return (NULL);
 }
