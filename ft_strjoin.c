7ea133f4ef8d88dfa383368038364cf03f5df9b0
KinGGuN73 <roms.laf@icloud.com>
2019-10-23 23:13:07 +0200

    🆕 ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_strjoin.c                                     .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: romainlaforgue <romainlaforgue@student.    +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/10/23 22:52:16 by romainlafor  #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/23 23:06:30 by romainlafor ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	size_t	x;
+	char	*res;
+
+	i = 0;
+	j = 0;
+	x = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+	if (!(res = malloc(sizeof(char) * (i + j) + 1)))
+		return (NULL);
+	i = 0;
+	while (s1[i])
+		res[x++] = s1[i++];
+	j = 0;
+	while (s2[j])
+		res[x++] = s2[j++];
+	res[x] = '\0';
+	return (res);
+}
--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                              /             */
 /*   libft.h                                          .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: rolaforg <rolaforg@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*   By: romainlaforgue <romainlaforgue@student.    +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/22 11:22:11 by rolaforg     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/22 11:22:58 by rolaforg    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/23 22:53:46 by romainlafor ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -37,6 +37,7 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strdup(const char *s1);
 char	*ft_strstr(const char *haystack, const char *needle);
