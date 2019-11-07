14e8cd36ada9df17eb3324c39312cd98134f85c5
Romain Laforgue <rolaforg@z3r4p2.le-101.fr>
2019-11-07 16:49:51 +0100

    Removed comment

--- ft_substr.c
+++ ft_substr.c
@@ -6,36 +6,10 @@
 /*   By: rolaforg <rolaforg@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/23 23:19:09 by romainlafor  #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/07 16:43:10 by rolaforg    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/07 16:49:35 by rolaforg    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
-
-#include "libft.h"
-
-// char	*ft_substr(char const *s, unsigned int start, size_t len)
-// {
-// 	char	*res;
-// 	size_t	i;
-
-// 	i = 0;
-// 	if (!s)
-// 		return (NULL);
-// 	if (!(res = malloc(sizeof(char) * (len + 1))))
-// 		return (NULL);
-// 	if (start < ft_strlen(s))
-// 	{
-// 		while (s[start + i] && i < len)
-// 		{
-// 			res[i] = s[start + i];
-// 			i++;
-// 		}
-// 		res[i] = '\0';
-// 	}
-// 	else
-// 		return ("");
-// 	return (res);
-// }
 
 #include "libft.h"
 
